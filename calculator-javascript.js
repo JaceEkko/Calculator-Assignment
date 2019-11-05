820a52ddd0d15583827829b160af5ce7c02a575f
John Knox <jgk305@nyu.edu>
2019-11-05 15:46:07 -0500

    added restyle functionality

--- calculator-javascript.js
+++ calculator-javascript.js
@@ -1,3 +1,4 @@
+//HALF BUTTON FUNCTIONALITY
 var button_half = document.getElementById("half-button");
 button_half.addEventListener("click", halfThis);
 
@@ -5,7 +6,7 @@ function halfThis() {
   var inputNum = document.getElementById("half-input").value;
   var inputNum_half = inputNum / 2;
 
-  if(inputNum){
+  if(inputNum){ //there is a value in this input
     alert("Half of " + inputNum + " is " + inputNum_half);
   }
   else{
@@ -13,19 +14,32 @@ function halfThis() {
   }
 };
 
+//FORTUNE BUTTON FUNCTIONALITY
 var button_fortune = document.getElementById("fortune-button");
 button_fortune.addEventListener("click", fortuneTeller);
 
 function fortuneTeller() {
-  var fortunes = ["Feel Enlightened", "Good Fortune is Coming", "You Miss ALL the Shots You Don't Take",
-                  "No One Can Be You, Except You", "Your Turn is Coming Up"];
-  var pick_fortune = fortunes[Math.floor(Math.random() * fortunes.length)]
+  var fortunes = [" Feel Enlightened", " Good Fortune is Coming", " You Miss ALL the Shots You Don't Take",
+                  " No One Can Be You, Except You", " Your Turn is Coming Up"];
+  var pick_fortune = fortunes[Math.floor(Math.random() * fortunes.length)] //the randopm fortune
 
   var inputName = document.getElementById("fortune-input").value;
-  if(inputName != ""){
+  var outputFortune = document.getElementById("fortune-output");
+  if(inputName != ""){ //there is a value in this input
+    outputFortune.innerHTML = pick_fortune;
     console.log(pick_fortune);
   }
   else{
     console.log("Please Enter Your Name");
   }
+};
+
+//RESTYLE BUTTON FUNCTIONALITY
+var button_style = document.getElementById("fortune-style-button");
+button_style.addEventListener("click", reStyle);
+
+function reStyle() {
+  var fortuneText = document.getElementById("fortune-output");
+  fortuneText.classList.toggle("FortuneText");
+  console.log(fortuneText.classList);
 };
